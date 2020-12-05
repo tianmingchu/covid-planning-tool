0a8f64f2bf841b82e1ba6079a0c275705ece38a4
Nayonika <nroy20@illinois.edu>
2020-12-04 20:41:43 -0600

    updates to graph.h

--- graph.h
+++ graph.h
@@ -1,43 +1,46 @@
 #pragma once
 
+#include <list>
+#include <unordered_map>
+#include <utility>
+#include <algorithm>
+#include <string>
+#include <cstdlib>
+#include <climits>
+#include <fstream>
+#include <iostream>
+#include <iomanip>
+#include <set>
+#include <sstream>
 #include <vector>
 #include <string>
-#include <utility>
-#include <map>
 
+#include "edge.h"
+
+using std::cerr;
+using std::cout;
+using std::endl;
+using std::set;
+using std::to_string;
+using std::make_pair;
+using std::unordered_map;
 using std::string;
 using std::vector;
 using std::pair;
-using std::map;
 
 class Graph {
     public: 
         void insertVertex(Vertex v);
         //void removeVertex(Vertex v);
-        void insertEdge(Vertex v1, Vertex v2);
+        void insertEdge(Vertex v1, Vertex v2, int cases);
         //void removeEdge(Vertex v1, Vertex v2);
         vector<Vertex> incidentVertices(Vertex v);
-        bool areAdjacent(Vertex v1, Vertex v2);
+        vector<Vertex> getVertices();
     private:
-        class Vertex {
-            public: 
-                Vertex(string name, int cases);
-                double getName();
-                int getCases();
-            private:
-                string name_;
-                int cases_;
-        };
         /*
-        class Edge {
-            public:
-                Edge(Vertex v1, Vertex v2);
-                pair<Vertex, Vertex> getVertices();
-            private:
-                pair<Vertex, Vertex> vertices_;
-        };
-        */
         vector<vector<bool>> edge_matrix_;
         map<Vertex, int> vertices_;
-        vector<Vertex> vertices_vector_;
+        vector<Vertex> vertices_vector_;*/
+
+        mutable unordered_map<Vertex, unordered_map<Vertex, Edge>> adjacency_list;
 };
