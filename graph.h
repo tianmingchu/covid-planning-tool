f242f432d7b45566aaa2e9b23bc88f94f9bb7306
Nayonika <nroy20@illinois.edu>
2020-12-10 19:10:40 -0600

    Added documentation to graph.h

--- graph.h
+++ graph.h
@@ -34,54 +34,87 @@ typedef pair<int, Vertex> intVertPair;
 class Graph {
     public: 
         /**
-         *  Prints out all states via BFS traversal
+         * @brief Prints out all states via BFS traversal
+         * 
+         * @param v The vertex to start with
          */
         void BFS(Vertex v);
 
         /**
-         *  Insert vertex into graph
+         * @brief Inserts a vertex representative of a state in the US into the graph
+         * 
+         * @param v The vertex to insert
          */
         void insertVertex(Vertex v);
 
         /**
-         *  Insert edge between two states weighted by
-         *  the sum of the cases between them. If a
-         *  vertex doesn't exist then one is created
+         * @brief Given two vertices and a weight, inserts an edge into the graph representing a path between two states and the number of cases representative of the path
+         * 
+         * @param v1 The starting vertex of the edge
+         * @param v2 The destination vertex of the edge
+         * @param cases The number of cases of the edge
          */
         void insertEdge(Vertex v1, Vertex v2, int cases);
 
         /**
-         *  Returns all vertices adjacent to a vertex
+         * @brief Given a vertex, returns all vertices adjacent to the specified vertex in a graph
+         * 
+         * @param v The vertex to return neighbors from
+         * @return vector<Vertex> A vector of adjacent vertices
          */
         vector<Vertex> incidentVertices(Vertex v);
 
         /**
-         *  Returns all vertices in the graph
+         * @brief Returns a vector containing all vertices in the graph
+         * 
+         * @return vector<Vertex> 
          */
         vector<Vertex> getVertices();
 
         /**
-         *  Returns all vertices in the graph
+         * @brief Returns whether the specified vertex exists in the graph
+         * 
+         * @param v The vertex to check
+         * @return true 
+         * @return false 
          */
         bool vertexExists(Vertex v) const;
 
+
         /**
-         *  
+         * @brief Returns a map containing source destination as the key and a vector containing the ordered vertices of the path to the destination as the value.
+         * 
+         * @param v1 Vertex representing the desired destination of the path
+         * @return map<Vertex, vector<Vertex>> 
          */
         map<Vertex, vector<Vertex>> shortestPaths(Vertex v1);
 
-        /**
-         *  
-         */
-        string landmarkPath(Vertex source, Vertex dest, Vertex landmark);
 
         /**
-         *  Return case rate between two states
+         * @brief Given a source, destination and landmark, returns a formatted path that starts at the source, ends at the destination, and includes the landmark vertex
+         * 
+         * @param source Vertex representing the desired starting point of the path
+         * @param dest Vertex representing the desired destination of the path
+         * @param landmark Vertex representing a point that must be included in the path
+         * @return string 
+         */
+         
+        string landmarkPath(Vertex source, Vertex dest, Vertex landmark);
+
+
+        /**
+         * @brief Returns case rate of the second vertex in the edge
+         * 
+         * @param v1 The starting point of the edge
+         * @param v2 The ending point of the edge
+         * @return int 
          */
         int getCasesByEdge(Vertex v1, Vertex v2);
 
+
         /**
-         *  Prints all vertices, their neighbors, and edges
+         * @brief Prints all vertices, their neighbors, and edges
+         * 
          */
         void printGraph();
 
--- main.cpp
+++ main.cpp
@@ -4,9 +4,9 @@
 int main() {
     
     Graph g = createGraph("united_states_covid19_cases_and_deaths_by_state.csv"); // create weighted graph based on data
-    g.BFS("Illinois"); // prints BFS traversal of graph
-    g.printGraph(); // prints all vertices, neighbors, & edges
-    
+    //g.BFS("Illinois"); // prints BFS traversal of graph
+    //g.printGraph(); // prints all vertices, neighbors, & edges
+    std::cout<<g.landmarkPath("Maine", "California", "North Dakota")<<std::endl;
     
     //nayonika's stuff
     /*
