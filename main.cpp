f11a30a844f7144204cb4f0a3d67298a29981998
Nayonika <nroy20@illinois.edu>
2020-12-10 21:02:32 -0600

    added extra test cases

--- main.cpp
+++ main.cpp
@@ -4,29 +4,7 @@
 int main() {
     
     Graph g = createGraph("united_states_covid19_cases_and_deaths_by_state.csv"); // create weighted graph based on data
-    //g.BFS("Illinois"); // prints BFS traversal of graph
-    //g.printGraph(); // prints all vertices, neighbors, & edges
+    g.BFS("Illinois"); // prints BFS traversal of graph
+    g.printGraph(); // prints all vertices, neighbors, & edges
     std::cout<<g.landmarkPath("Maine", "California", "North Dakota")<<std::endl;
-    
-    //nayonika's stuff
-    /*
-    Graph g;
-    Vertex v1 = "illinois";
-    Vertex v2 = "wisconsin";
-    Vertex v3 = "minnesota";
-    Vertex v4 = "north dakota";
-    Vertex v5 = "south dakota";
-    g.insertVertex(v1);
-    g.insertVertex(v2);
-    g.insertVertex(v3);
-    g.insertVertex(v4);
-    g.insertVertex(v5);
-    g.insertEdge(v1, v2, 300);
-    g.insertEdge(v2, v3, 200);
-    g.insertEdge(v3, v4, 50);
-    g.insertEdge(v4, v5, 50);
-    g.insertEdge(v2, v5, 400);
-    g.insertEdge(v1, v5, 100);
-    std::cout << g.landmarkPath(v1, v5, v3) << std::endl;
-    */
 }
--- tests/tests.cpp
+++ tests/tests.cpp
@@ -40,3 +40,45 @@ TEST_CASE("Graph_test_edges") {
     REQUIRE(g.incidentVertices(v1).size() == 2);
     REQUIRE(g.incidentVertices(v3).size() == 1);
 }
+
+TEST_CASE("Graph_shortest_path") {
+    Graph g;
+    Vertex v1 = "illinois";
+    Vertex v2 = "wisconsin";
+    g.insertVertex(v1);
+    g.insertVertex(v2);
+    g.insertEdge(v1, v2, 50);
+    map<Vertex, vector<Vertex>> paths = g.shortestPaths(v2);
+    REQUIRE(paths[v1][0] == "illinois");
+    REQUIRE(paths[v1][1] == "wisconsin");
+    Vertex v3 = "minnesota";
+    g.insertEdge(v2, v3, 100);
+    g.insertEdge(v1, v3, 100);
+    paths = g.shortestPaths(v3);
+    REQUIRE(paths[v1][0] == "illinois");
+    REQUIRE(paths[v1][1] == "minnesota");
+}
+
+TEST_CASE("Graph_landmark_path") {
+    Graph g;
+    Vertex v1 = "illinois";
+    Vertex v2 = "wisconsin";
+    Vertex v3 = "minnesota";
+    Vertex v4 = "north dakota";
+    Vertex v5 = "south dakota";
+    g.insertVertex(v1);
+    g.insertVertex(v2);
+    g.insertVertex(v3);
+    g.insertVertex(v4);
+    g.insertVertex(v5);
+    g.insertEdge(v1, v2, 300);
+    g.insertEdge(v2, v3, 200);
+    g.insertEdge(v3, v4, 50);
+    g.insertEdge(v4, v5, 50);
+    REQUIRE(g.landmarkPath(v1, v5, v3) == "Path: illinois->wisconsin->minnesota->north dakota->south dakota");
+    g.insertEdge(v2, v5, 400);
+    g.insertEdge(v1, v5, 100);
+    REQUIRE(g.landmarkPath(v1, v5, v3) == "Path: illinois->south dakota->north dakota->minnesota->north dakota->south dakota");
+    g.insertEdge(v1, v3, 10);
+    REQUIRE(g.landmarkPath(v1, v5, v3) == "Path: illinois->minnesota->north dakota->south dakota");
+}
